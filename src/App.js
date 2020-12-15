e7d9c9e2efd332101deccde9c7601712aca862bf
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-12-15 23:06:50 +0200

    verstka

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React from "react";
+import Minus from "./buttons/minus"
+import Pause from "./buttons/pause";
+import Play from "./buttons/play";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Tracker</h1>
-      <div className="wrapper">
-        <div className="form">
-          <input type="text" />
-          <button></button>
+    let flag = false;
+    const toggle = () => {
+        flag = !flag
+    }
+    let btn = flag ? <Play /> : <Pause />;
+
+    return (
+        <div className="container">
+            <h1>Tracker</h1>
+            <div className="wrapper">
+                <div className="form">
+                    <input onClick={toggle} type="text" placeholder="Enter tracker name"/>
+                    <Play color="green" width="50"/>
+                </div>
+                <div className="track">
+                    <h3>Title</h3>
+                    <h4>Time</h4>
+                    { btn }
+                    <Minus/>
+                </div>
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {render} from 'react-dom';
+// import {Provider} from 'react-redux';
+// import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,15 +9,12 @@ import reportWebVitals from './reportWebVitals';
 // const store = createStore(rootReducer);
 
 const app = (
-  <Provider>
-  <App />
-  </Provider>
-  );
-  
-  render(app, document.getElementById('root'));
-  
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
-  
+        <App/>
+);
+
+render(app, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
