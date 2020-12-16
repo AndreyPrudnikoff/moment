6ceac9917985bae122c8a1fd82d78da56722a65e
AndreyPrudnikoff <a.prudniks@gmail.com>
2020-12-16 11:39:57 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import React from "react";
+import {connect} from "react-redux";
+import moment from 'moment';
 import Minus from "./buttons/minus"
 import Pause from "./buttons/pause";
 import Play from "./buttons/play";
 
-function App() {
-    let flag = false;
-    const toggle = () => {
-        flag = !flag
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {flag: false};
     }
-    let btn = flag ? <Play /> : <Pause />;
 
-    return (
-        <div className="container">
-            <h1>Tracker</h1>
-            <div className="wrapper">
-                <div className="form">
-                    <input onClick={toggle} type="text" placeholder="Enter tracker name"/>
-                    <Play color="green" width="50"/>
+    toggle = () => {
+        console.log()
+        this.setState({flag: this.flag = !this.flag});
+    }
+
+    render() {
+        // let {flag} = this.state;
+        // let btn = flag ? <Pause/> : <Play/>;
+        return (
+            <div className="container">
+                <h1>Tracker</h1>
+                <div className="wrapper">
+                    <div className="form">
+                        <input type="text" placeholder="Enter tracker name"/>
+                        <button><Play color="green" width="50"/></button>
+                    </div>
+                    <div className="track-list">
+                        {this.props.playlist.map((track, index) => (
+                            <div key={ index } className="track">
+                                <h3>{ track.title }</h3>
+                                <h4>{ track.time }</h4>
+                                <div>
+                                    <button onClick={this.toggle}>{ track.playing ? <Pause/> : <Play/> }</button>
+                                    <button><Minus/></button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
                 </div>
-                <div className="track">
-                    <h3>Title</h3>
-                    <h4>Time</h4>
-                    { btn }
-                    <Minus/>
-                </div>
+
             </div>
+        );
+    }
 
-        </div>
-    );
 }
 
-export default App;
+export default connect(
+    state => ({
+        playlist: state
+    }),
+    dispatch => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import {render} from 'react-dom';
-// import {Provider} from 'react-redux';
-// import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import moment from "moment";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const store = createStore(rootReducer);
+localStorage.setItem('playList', JSON.stringify([
+    {title: "Song1", time: moment().format("DD MM YYYY hh:mm:ss"), playing: false},
+    {title: "Song2", time: moment().format("DD MM YYYY hh:mm:ss"), playing: false},
+    {title: "Song3", time: moment().format("DD MM YYYY hh:mm:ss"), playing: false}
+]))
+
+const initialState = JSON.parse(localStorage.getItem('playList')) || [];
+
+const playlist = (state = initialState, action) => {
+    if (action.type === 'ADD_TRACK') {
+        return [...state, action.payload]
+    }
+    return state
+}
+
+const store = createStore(playlist);
 
 const app = (
+    <Provider store={store}>
         <App/>
+    </Provider>
 );
 
 render(app, document.getElementById('root'));
